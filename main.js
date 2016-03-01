29a0a3341a8bc5cfb122c1e2939de247b7a63760
FIRST_NAME LAST_NAME <YOUR_EMAIL>
2016-03-01 14:10:57 -0500

    declaring variables and pulling input...

---
+++ main.js
@@ -0,0 +1,20 @@
+var x 
+var y 
+var calculate  
+var answer
+
+
+
+ x = document.querySelector(#num_one);
+ y = document.querySelector(#num_two);
+ calculate = document.querySelector(#calculateBtn);
+ answer = document.querySelector(#answer);
+
+
+
+
+function = multiplyNumbers (x,y) {
+	return multiplyNumbers = x * y; 
+}
+
+var answer = multiplyNumbers(x,y);
